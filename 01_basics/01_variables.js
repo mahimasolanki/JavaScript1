0fafec64d6c9035efb1511e5a07ddd5ccb4a7460
mahimasolanki <142207567+mahimasolanki@users.noreply.github.com>
2024-01-16 07:48:18 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "mahi345@gmail.com"
+var accountPassword = "1122"
+acountCity = "Jaipur"
+let accountState;
+
+// accountId = 22  // mot allowed
+
+console.log(accountId);
+
+accountEmail = "ma11@gmail.com"
+accountPassword = "3344"
+acountCity = "Surat"
+
+console.table([accountId, accountEmail, accountPassword, acountCity, accountState]);
